f2e36fbb71edb4efc89ec288530b49a69aa5a5a8
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2020-12-09 12:16:26 +0000

    Create card-snowflakes-trinket.io-Version.py

---
+++ xmas-card-design/code/card-snowflakes-trinket.io-Version.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import turtle
+elsa = turtle.Turtle()
+turtle.Screen().bgcolor("#ECEFF8")
+elsa.color("#78ADCE")
+for i in range(10):
+    for i in range(2):
+        elsa.forward(100)
+        elsa.right(60)
+        elsa.forward(100)
+        elsa.right(120)
+    elsa.right(36)
+
+elsa.hideturtle()
