421487723bbc7cf0bd5642339ccb87f077d41af1
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2020-12-09 12:15:54 +0000

    Create step-6.py

---
+++ tutorial/code/step-6.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import turtle
+elsa = turtle.Turtle()
+for i in range(10):
+    for i in range(2):
+        elsa.forward(100)
+        elsa.right(60)
+        elsa.forward(100)
+        elsa.right(120)
+        elsa.right(36)
